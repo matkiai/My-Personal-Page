8c6b2533337f01b14ce418426029efad4688b6f8
matkiai <101467033+matkiai@users.noreply.github.com>
2023-09-25 12:37:21 -0400

    2 commit

---
+++ script1.js
@@ -0,0 +1,22 @@
+// Write your code below
+const items = document.querySelectorAll('.item:not(:first-child)');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    } else { // this else part is not compulsory
+      entry.target.classList.remove('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
